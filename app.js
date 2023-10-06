d5e3cb91a47f3057face24ab0ba7b5de6066be40
afshan ahmed khan <afshanahmeda2k@gmail.com>
2023-10-06 23:28:47 +0530

    Added lodash for memoization

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use(function(req, res, next) {
 });
 
 app.use(cors())
-// app.use(express.json())
 app.use(router)
 app.listen(process.env.PORT,()=>{
     console.log("server is running on "+process.env.PORT)
--- controllers/index.js
+++ controllers/index.js
@@ -1,24 +1,49 @@
 const express = require('express')
 const router = express.Router()
 const _ = require('lodash');
-const axios = require('axios')
+const axios = require('axios');
 
-router.get("/api/blog-status", async (req, res) => {
-    const url = process.env.URL;
+// cache the get blog status request using lodash memoize function 
+const getBlogs =  async () => {
+    const url = process.env.URL 
     const headers = {
         'x-hasura-admin-secret': process.env.SECRET,
     };
+    const blogs = (await axios.get(url, { headers })).data.blogs
+    console.log("Out of cache")
+    return blogs
+}
+const getBlogStatus = _.memoize(async () => {
+    // const url = process.env.URL;
+    const blogs = await getBlogs()
+    const response = {
+        "Total Number of blogs": _.size(blogs),
+        "Title of Longest blog": _.maxBy(blogs, (blog) => blog.title.length),
+        "Number of blogs with privacy": _.filter(blogs, (blog) => {
+            return _.includes(blog.title.toLowerCase(), 'privacy')
+        }),
+        "Arry of unique blog titles": _.uniqBy(blogs, 'title')
+    }
+    return response
+})
+
+const getSearch = _.memoize(async (query) => {
+    const blogs = await getBlogs() 
+    const searchResults = _.filter(blogs, (blog) =>
+        _.includes(blog.title.toLowerCase(), query)
+    );
+    return searchResults
+})
+
+router.get("/api/blog-status", async (req, res) => {
     try {
-        const blogs = (await axios.get(url, { headers })).data.blogs
-        res.status(200).json({
-            "Total Number of blogs": _.size(blogs),
-            "Title of Longest blog": _.maxBy(blogs, (blog) => blog.title.length),
-            "Number of blogs with privacy": _.filter(blogs, (blog) => {
-                return _.includes(blog.title.toLowerCase(), 'privacy')
-            }),
-            "Arry of unique blog titles": _.uniqBy(blogs, 'title')
-        })
+        let status = await getBlogStatus()
+        setTimeout(() => {
+            if (getBlogStatus.cache.size > 0) getBlogStatus.cache.clear()
+        }, Number(process.env.CLEARAFTER))
+        res.status(200).json(status)
     } catch (error) {
+        if (getBlogStatus.cache.size > 0) getBlogStatus.cache.clear()
         console.error('Axios error:', error);
         if (error.response) {
             res.status(error.response.status).json(error.response.data)
@@ -26,19 +51,17 @@ router.get("/api/blog-status", async (req, res) => {
         else res.status(500).json({ message: "Internal Server Error" })
     }
 })
+
 router.get('/api/blog-search', async (req, res) => {
-    const url = process.env.URL;
-    const headers = {
-        'x-hasura-admin-secret': process.env.SECRET,
-    };
     try {
-        const blogs = (await axios.get(url, { headers })).data.blogs
-        const query = req.query.query.toLowerCase();
-        const searchResults = _.filter(blogs, (blog) =>
-            _.includes(blog.title.toLowerCase(), query)
-        );
+        // let searchResults = await getSearch
+        let searchResults = await getSearch(req.query.query.toLowerCase()) 
+        setTimeout(()=>{
+            if(getSearch.cache.size > 0) getSearch.cache.clear()
+        }, Number(process.env.CLEARAFTER))
         res.status(200).json(searchResults);
     } catch (error) {
+        if (getBlogStatus.cache.size > 0) getBlogStatus.cache.clear()
         console.error('Axios error:', error);
         if (error.response) {
             res.status(error.response.status).json(error.response.data)
